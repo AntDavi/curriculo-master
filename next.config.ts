518fd5916239878a768442ca5a3f6227e9015b69
Anthony Davi <ant.davi07@gmail.com>
2025-03-26 17:42:52 -0300

    feat: add all sections with drag and drop mechanic for the all resume complements

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,19 @@
+import { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-      turbo: {
-          rules: {
-              "*.svg": {
-                  loaders: ["@svgr/webpack"],
-                  as: "*.js",
-              },
-          },
-      },
-  },
-  reactStrictMode: false,
+    /* config options here */
+    experimental: {
+        turbo: {
+            rules: {
+                "*.svg": {
+                    loaders: ["@svgr/webpack"],
+                    as: "*.js",
+                },
+            },
+        },
+    },
+    reactStrictMode: false,
+    devIndicators: false
 };
 
 
